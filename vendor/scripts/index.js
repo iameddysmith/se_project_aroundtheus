316e5c73a6557572fcd0bef5430d0703b1d96149
Eddy Smith <iameddysmith@gmail.com>
2024-03-31 08:00:29 -0700

    Project 4: Phase 1

---
+++ vendor/scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Mar Caribe",
+    link: "https://images.unsplash.com/photo-1538964173425-93884d739596?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Mount Shasta",
+    link: "https://images.unsplash.com/photo-1581881067989-7e3eaf45f4f6?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Death Valley",
+    link: "https://images.unsplash.com/photo-1559767180-47d8f4919e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc2VydHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    name: "Bagby Hot Springs",
+    link: "https://images.unsplash.com/photo-1533657794-ad5a8ae38c55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFnYnl8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Amazon River",
+    link: "https://images.unsplash.com/photo-1598837218686-a456fdaa5cf3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW1hem9uJTIwcml2ZXJ8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Cairo",
+    link: "https://images.unsplash.com/photo-1572252009286-268acec5ca0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWd5cHR8ZW58MHx8MHx8fDA%3D",
+  },
+];
